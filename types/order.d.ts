920a99817736dc6aa70a25d406ef6e536f4d9675
joamag <joamag@gmail.com>
2020-06-03 23:27:16 +0100

    New interfaces

--- types/order.d.ts
+++ types/order.d.ts
@@ -1,4 +1,10 @@
+export interface Order {
+    readonly number: number,
+    readonly ff_order_id: string,
+    readonly [index: string]: any;
+}
+
 export declare class OrderAPI {
-    getOrder(number: number): object;
+    getOrder(number: number): Order;
     reportOrder(number: number): Blob;
 }
