84a7e446c12c4f94b2733484f77835feedb1b79b
João Magalhães <joamag@gmail.com>
2021-01-15 16:20:15 +0000

    feat: support for commonjs in rollup

--- rollup.config.js
+++ rollup.config.js
@@ -67,6 +67,7 @@ export default [
             }
         ],
         plugins: [
+            commonjs(),
             resolve({
                 customResolveOptions: {
                     paths: nodePath
